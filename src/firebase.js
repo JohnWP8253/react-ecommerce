4bb0f8b96550fdbb6ec0db81cc294f5b50d21e4f
John Pendergrass <john.pendergrass@me.com>
2021-01-26 10:04:22 -0600

    added Firebase configuration to firebase.js and initial commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC17wn0GQpcHwXoI4lYelMKm9fwY8wjk2Y",
+  authDomain: "challenge-308a9.firebaseapp.com",
+  projectId: "challenge-308a9",
+  storageBucket: "challenge-308a9.appspot.com",
+  messagingSenderId: "824449873288",
+  appId: "1:824449873288:web:43a21f38fafb31cf10363d",
+  measurementId: "G-QJ7PS5DXX7"
+};
