31e7f7df70a06bb66d11d5be691a554b33549539
John Pendergrass <john.pendergrass@me.com>
2021-02-16 09:39:39 -0600

    moved header component outside of the Switch class to show on all routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+// imported components
 import Header from "./components/Header/Header.jsx";
 import Home from "./components/Home/Home.jsx";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Checkout from "./components/Checkout/Checkout.jsx";
 
 function App() {
   return (
     // BEM
     <Router>
       <div className="App">
+        {/* Header is always displayed*/}
+        <Header />
         <Switch>
           {/* Checkout route */}
           <Route path="/checkout">
-            {/* Header */}
-            <Header />
-            <h1>I am a checkout, smash the like button</h1>
+            <Checkout />
           </Route>
           {/* Landing route */}
           <Route path="/">
-            {/* Header */}
-            <Header />
             {/* Home */}
             <Home />
           </Route>
