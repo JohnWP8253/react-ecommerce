f6fdfe47871ee01fbdd389a072576f008da49a7e
John Pendergrass <john.pendergrass@me.com>
2021-02-16 09:32:17 -0600

    created simple routes for checkout and homepage

--- src/App.js
+++ src/App.js
@@ -1,19 +1,30 @@
 import "./App.css";
 import Header from "./components/Header/Header.jsx";
 import Home from "./components/Home/Home.jsx";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     // BEM
-    <div className="App">
-      {/* Header */}
-      <Header/>
-      {/* Home */}
-      <Home />
-
-    </div>
-
-
+    <Router>
+      <div className="App">
+        <Switch>
+          {/* Checkout route */}
+          <Route path="/checkout">
+            {/* Header */}
+            <Header />
+            <h1>I am a checkout, smash the like button</h1>
+          </Route>
+          {/* Landing route */}
+          <Route path="/">
+            {/* Header */}
+            <Header />
+            {/* Home */}
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
