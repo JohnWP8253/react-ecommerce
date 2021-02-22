9fbb9acfbbb54a41c303f5c9b660d7986c9338a0
John Pendergrass <john.pendergrass@me.com>
2021-02-22 15:54:36 -0600

    added basket code for the basket section

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -1,15 +1,18 @@
+// Import statements for the dataLayer
 import React, {
   createContext,
   useContext,
   useReducer,
 } from "react";
 
+// Prepares the dataLayer
 export const StateContext = createContext();
-
+// Wrap the app and provide the data layer to all components in the app
 export const StateProvider = ({ reducer, initialState, children }) => (
   <StateContext.Provider value={useReducer(reducer, initialState)}>
     {children}
   </StateContext.Provider>
 );
 
+// code to pull information from the data layer
 export const useStateValue = () => useContext(StateContext);
