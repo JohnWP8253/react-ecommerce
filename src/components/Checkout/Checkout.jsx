fb015303a0bfa9b91a3b20a636e9af64b15a461c
John Pendergrass <john.pendergrass@me.com>
2021-04-07 13:23:49 -0500

    added CheckoutProduct component and added it to the Checkout component using reducer

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import "./Checkout.css";
-import Subtotal from "../Subtotal/Subtotal"
+import Subtotal from "../Subtotal/Subtotal";
+import { useStateValue } from "../../StateProvider";
+import CheckoutProduct from "../CheckoutProduct/CheckoutProduct";
 
 function Checkout() {
+    const [{ basket }, dispatch] = useStateValue();
     return (
         <div className="checkout">
             {/* left-side of checkout page */}
@@ -12,11 +15,16 @@ function Checkout() {
                     <h2 className="checkout__title">
                         Your shopping basket
                     </h2>
-                    {/* basket item */}
-                    {/* basket item */}
-                    {/* basket item */}
-                    {/* basket item */}
-                    {/* basket item */}
+                    {/* Checkout project */}
+                    { basket.map(item => (
+                        <CheckoutProduct
+                        id={item.id}
+                        title={item.title}
+                        image={item.image}
+                        price={item.price}
+                        rating={item.rating}
+                        />
+                    ))}
                 </div>
             </div>
             {/* right-side of checkout page */}
