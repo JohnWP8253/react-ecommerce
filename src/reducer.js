6f1ff7dd98d7e6370851732401e1a713772ae3f7
John Pendergrass <john.pendergrass@me.com>
2021-04-07 09:48:51 -0500

    added getBasketTotal from reducer to the subtotal page and is working

--- src/reducer.js
+++ src/reducer.js
@@ -1,9 +1,15 @@
 export const initialState = {
   basket: [],
+  user: null,
 };
 
+// Selector
+export const getBasketTotal = (basket) =>
+  // setting the initial amount to 0 and tallies up all of the items and their price
+  basket?.reduce((amount, item) => item.price + amount, 0);
+
 const reducer = (state, action) => {
-    console.log(action);
+  console.log(action);
   // eslint-disable-next-line default-case
   switch (action.type) {
     case "ADD_TO_BASKET":
@@ -11,6 +17,8 @@ const reducer = (state, action) => {
         ...state,
         basket: [...state.basket, action.item],
       };
+    default:
+      return state;
   }
 };
 
