b88476aa2f6a9d6b4456c3e811f1c20e02da97d9
John Pendergrass <john.pendergrass@me.com>
2021-06-14 10:18:57 -0500

    created code to authenticate user who is signed up and send to home page

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,9 @@
 import React, { useState } from "react";
 import "./Login.css";
 import { Link, useHistory } from "react-router-dom";
-import { auth } from '../../firebase';
+import { auth } from "../../firebase";
 
 function Login() {
-
   const history = useHistory();
 
   const [email, setEmail] = useState("");
@@ -14,20 +13,28 @@ function Login() {
     // stops page from refeshing
     e.preventDefault();
 
-    // firebase logic will be coded here.
+    // firebase logic here.
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((auth) => {
+        history.push("/");
+      })
+      .catch((error) => alert(error.message));
   };
 
   const register = (e) => {
     e.preventDefault();
     // firebase logic coded here.
-    auth.createUserWithEmailAndPassword(email, password)
-    .then((auth) => {
-      console.log(auth);
-      if (auth) {
-        history.push('/')
-      }
-    })
-    .catch(error => alert(error.message))
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((auth) => {
+        console.log(auth);
+        if (auth) {
+          // if authenticated, sends to homepage
+          history.push("/");
+        }
+      })
+      .catch((error) => alert(error.message));
   };
 
   return (
